dc4136f185a4ec49ed731a3e22bab4c545afbc4c
Mpira123 <55028339+Mpira123@users.noreply.github.com>
2020-01-24 23:13:28 +0200

    Add files via upload

---
+++ Queue/Queue/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+
+namespace Queue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var easyQueue = new EasyQueue<int>();
+            easyQueue.Enqueue(1);
+            easyQueue.Enqueue(2);
+            easyQueue.Enqueue(3);
+            easyQueue.Enqueue(4);
+            easyQueue.Enqueue(5);
+            easyQueue.Enqueue(6);
+            easyQueue.Enqueue(7);
+            Console.WriteLine(easyQueue.Dequeue());
+            Console.WriteLine(easyQueue.Peek());
+            Console.WriteLine(easyQueue.Dequeue());
+            Console.ReadLine();
+        }
+    }
+}
